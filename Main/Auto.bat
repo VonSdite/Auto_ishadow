48a680d56fae54d7af90330622f54567da5ce1b5
Little Von <122691411@qq.com>
2018-03-05 23:51:55 +0800

    简化批处理

--- Main/Auto.bat
+++ Main/Auto.bat
@@ -17,41 +17,5 @@ echo ----------------------7. Usa-A-Server----------------------
 echo.
 CHOICE /C 1234567 /M Please--Input--the--Server--Num
 
-if %errorlevel%==1 goto JapanA
-if %errorlevel%==2 goto JapanB
-if %errorlevel%==3 goto JapanC
-if %errorlevel%==4 goto SingaporeA
-if %errorlevel%==5 goto SingaporeB
-if %errorlevel%==6 goto SingaporeC
-if %errorlevel%==7 goto UsaA
-
-:JapanA
-run_shadowsocks.py 1
-goto end
-
-:JapanB
-run_shadowsocks.py 2
-goto end
-
-:JapanC
-run_shadowsocks.py 3
-goto end
-
-:SingaporeA
-run_shadowsocks.py 4
-goto end
-
-:SingaporeB
-run_shadowsocks.py 5
-goto end
-
-:SingaporeC
-run_shadowsocks.py 6
-goto end
-
-:UsaA
-run_shadowsocks.py 7
-goto end
-
-:end
+run_shadowsocks.py %errorlevel%
 pause
